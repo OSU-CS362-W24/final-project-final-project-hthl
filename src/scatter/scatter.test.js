2d613db1df324ef7f97929fd9cbc9b765ef9c2ac
Tran Thi Que Tran <quetran.tran1009@gmail.com>
2024-03-17 13:27:21 -0700

    remove isolate modules

--- src/scatter/scatter.test.js
+++ src/scatter/scatter.test.js
@@ -1,11 +1,11 @@
 /**
  * @jest-environment jsdom
  */
-const generate = require('../lib/generateChartImg');
 
+const chartStorage = require('../lib/chartStorage.js');
 const fs = require('fs');
 const domTesting = require('@testing-library/dom');
-const { beforeEach } = require('node:test');
+
 require('@testing-library/jest-dom');
 const userEvent = require('@testing-library/user-event').default;
 
@@ -15,7 +15,6 @@ function initDomFromFiles(htmlPath, jsPath) {
   document.write(html);
   document.close();
   require(jsPath);
-
 }
 /* Write one or more integration
 tests to verify that the “add values” button (i.e. the “+” button) in the sidebar of the
@@ -24,111 +23,109 @@ add a new pair of input fields for the user to enter new X and Y values. Clickin
 the button should not impact any data the user has already entered.*/
 
 describe('Test for adding values', function () {
- 
-  // test('A new pair of input fields for the user is added everytime the user clicks + button', async function () {
-
-  //   initDomFromFiles(`${__dirname}/scatter.html`, `${__dirname}/scatter.js`);
-  //   const addButton = domTesting.getByText(document, '+');
-  //   console.log(addButton);
-  //   const user = userEvent.setup();
-  //   for (let i = 0; i < 3; ++i) {
-  //     const prevNumsXFields = domTesting.queryAllByLabelText(document, 'X', {
-  //       exact: true,
-  //     }).length;
-  //     const prevNumsYFields = domTesting.queryAllByLabelText(document, 'Y', {
-  //       exact: true,
-  //     }).length;
-
-  //     expect(prevNumsXFields).toEqual(prevNumsYFields);
-  //     await user.click(addButton);
-  //     const allYFields = domTesting.queryAllByLabelText(document, 'Y', {
-  //       exact: true,
-  //     });
-  //     const allXFields = domTesting.queryAllByLabelText(document, 'X', {
-  //       exact: true,
-  //     });
-
-  //     expect(allYFields).toHaveLength(prevNumsYFields + 1);
-  //     expect(allXFields).toHaveLength(prevNumsXFields + 1);
-  //   }
-  // });
-  // test('clicking the button doesn`t impact the old data', async function () {
-
-  //   initDomFromFiles(`${__dirname}/scatter.html`, `${__dirname}/scatter.js`);
-  //   const addButton = domTesting.getByText(document, '+');
-  //   const user = userEvent.setup();
-  //   const [X, Y] = [10, 9]
-  //   const prevXFields = domTesting.getByLabelText(document, 'X', {
-  //     exact: true,
-  //     selector: 'input'
-  //   })
-  //   const prevYFields = domTesting.getByLabelText(document, 'Y', {
-  //     exact: true,
-  //     selector: 'input'
-  //   })
-  //   console.log(prevXFields)
-  
-  //   await user.type(prevXFields, JSON.stringify(X))
-  //   await user.type(prevYFields, JSON.stringify(Y))
-  //   await user.click(addButton)
-  //   expect(domTesting.queryAllByLabelText(document, 'X', {
-  //     exact: true,
-  //     selector: 'input'
-  //   }).firstElementChild).toHaveValue(X)
-  //   expect(domTesting.queryAllByLabelText(document, 'Y', {
-  //     exact: true,
-  //     selector: 'input'
-  //   }).firstElementChild).toHaveValue(Y)
-
-
-  // });
   beforeEach(function () {
     jest.resetModules();
   });
-  test('generate chart image', async function(){
-    const obj = {
-      fn: generate
-    }
-    const user = userEvent.setup();
+  test('A new pair of input fields for the user is added everytime the user clicks + button', async function () {
     initDomFromFiles(`${__dirname}/scatter.html`, `${__dirname}/scatter.js`);
-    jest.mock("../lib/generateChartImg.js")
+    const addButton = domTesting.getByText(document, '+');
+    const user = userEvent.setup();
+    for (let i = 0; i < 3; ++i) {
+      const prevNumsXFields = domTesting.queryAllByLabelText(document, 'X', {
+        exact: true,
+      }).length;
+      const prevNumsYFields = domTesting.queryAllByLabelText(document, 'Y', {
+        exact: true,
+      }).length;
 
-const generateChartImgSpy = require("../lib/generateChartImg.js")
+      expect(prevNumsXFields).toEqual(prevNumsYFields);
+      await user.click(addButton);
+      const allYFields = domTesting.queryAllByLabelText(document, 'Y', {
+        exact: true,
+      });
+      const allXFields = domTesting.queryAllByLabelText(document, 'X', {
+        exact: true,
+      });
+
+      expect(allYFields).toHaveLength(prevNumsYFields + 1);
+      expect(allXFields).toHaveLength(prevNumsXFields + 1);
+    }
+  });
+  beforeEach(function () {
+    jest.resetModules();
+  });
+
+  test('clicking the + button doesn`t impact the old data', async function () {
+    initDomFromFiles(`${__dirname}/scatter.html`, `${__dirname}/scatter.js`);
+    const user = userEvent.setup();
+    const addButton = domTesting.getByText(document, '+');
+    const [X, Y] = [10, 9];
+    const prevXFields = domTesting.getByLabelText(document, 'X', {
+      exact: true,
+      selector: 'input',
+    });
+    const prevYFields = domTesting.getByLabelText(document, 'Y', {
+      exact: true,
+      selector: 'input',
+    });
+
+    await user.type(prevXFields, JSON.stringify(X));
+    await user.type(prevYFields, JSON.stringify(Y));
+    await user.click(addButton);
+    expect(
+      domTesting.getAllByLabelText(document, 'X', {
+        exact: true,
+        selector: 'input',
+      })[0]
+    ).toHaveValue(X);
+    expect(
+      domTesting.queryAllByLabelText(document, 'Y', {
+        exact: true,
+        selector: 'input',
+      })[0]
+    ).toHaveValue(Y);
+  });
+  beforeEach(function () {
+    jest.resetModules();
+    chartStorage.updateCurrentChartData({});
+  });
+  test('generate chart image correctly', async function () {
+    initDomFromFiles(`${__dirname}/scatter.html`, `${__dirname}/scatter.js`);
+
+    const user = userEvent.setup();
+    jest.mock('../lib/generateChartImg.js');
+    const generateChartImgSpy = require('../lib/generateChartImg.js');
 
     const input = {
-     title: 'Scatter',
-     data: [{ x: 0, y: 0 }],
-     xLabel: 'X',
-     type:'scatter',
-     yLabel: 'Y'
-    }
+      title: 'Scatter',
+      data: [{ x: 0, y: 0 }],
+      xLabel: 'X',
+      type: 'scatter',
+      yLabel: 'Y',
+    };
 
-    const title = domTesting.getByLabelText(document, 'Chart title')
+    const title = domTesting.getByLabelText(document, 'Chart title');
     const xField = domTesting.getByLabelText(document, 'X', {
-      exact: true
-    })
+      exact: true,
+    });
     const yField = domTesting.getByLabelText(document, 'Y', {
-      exact: true
-    })
+      exact: true,
+    });
     const xLabel = domTesting.getByLabelText(document, 'X label', {
-      exact: true
-    })
+      exact: true,
+    });
     const yLabel = domTesting.getByLabelText(document, 'Y label', {
-      exact: true
-    })
-    const genBtn = domTesting.getByText(document, 'Generate chart')
-  
-    
-  
-    await user.type(title, input.title)
-    await user.type(xLabel, input.xLabel)
-    await user.type(yLabel, input.yLabel)
-    await user.type(xField, JSON.stringify(input.data[0].x))
-    await user.type(yField, JSON.stringify(input.data[0].y))
-    await user.click(genBtn)
+      exact: true,
+    });
+    const genBtn = domTesting.getByText(document, 'Generate chart');
+
+    await user.type(title, input.title);
+    await user.type(xLabel, input.xLabel);
+    await user.type(yLabel, input.yLabel);
+    await user.type(xField, JSON.stringify(input.data[0].x));
+    await user.type(yField, JSON.stringify(input.data[0].y));
+    await user.click(genBtn);
     expect(generateChartImgSpy).toHaveBeenCalled();
-    generateChartImgSpy.mockRestore()
-  })
+    generateChartImgSpy.mockRestore();
+  });
 });
-
-
