fe31be95ef3e7394d5899a0ad792c462e6021158
ajh416 <henryada@oregonstate.edu>
2024-03-21 14:53:40 -0700

    add test for retrieving a specific chart

--- src/__tests__/chartStorage.test.js
+++ src/__tests__/chartStorage.test.js
@@ -41,27 +41,4 @@ describe('chartStorage', () => {
 
     expect(chartStorage.loadSavedChart(1)).toEqual(chart2);
   });
-
-  it('should retrieve all charts', () => {
-    const chart1 = { id: 'chart-1', data: [1, 2, 3] };
-    const chart2 = { id: 'chart-2', data: [4, 5, 6] };
-    chartStorage.saveChart(chart1);
-    chartStorage.saveChart(chart2);
-
-    expect(chartStorage.loadAllSavedCharts()).toEqual([chart1, chart2]);
-  });
-
-  it('should update the current chart data', () => {
-    const chart = { id: 'chart-1', data: [1, 2, 3] };
-    chartStorage.updateCurrentChartData(chart);
-
-    expect(JSON.parse(window.localStorage.getItem('currentChartData'))).toEqual(chart);
-  });
-
-  it('should retrieve the current chart data', () => {
-    const chart = { id: 'chart-1', data: [1, 2, 3] };
-    chartStorage.updateCurrentChartData(chart);
-
-    expect(chartStorage.loadCurrentChartData()).toEqual(chart);
-  });
 });
